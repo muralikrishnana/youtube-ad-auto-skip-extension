12d209a5b9c0bb31ab529e49510a6db796a5d315
Unni <amuralikrishnan@outlook.com>
2019-05-22 13:07:13 +0530

    added optional voice detection feature for play/pause videos. still in alpha stage. paerformance issues may arise if turned on.

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # youtube-ad-auto-skip-extension
+
+voice detection feature is in alpha stage. must turn on in contentscript inorder to use.
--- chrome/Skip - The Youtube AD Auto Skipper/contentscript.js
+++ chrome/Skip - The Youtube AD Auto Skipper/contentscript.js
@@ -1,3 +1,7 @@
+const config = {
+  speech: false
+};
+
 const mouseClick = new MouseEvent("click", {
   view: window,
   bubbles: true,
@@ -39,7 +43,66 @@ const handleAddNode = event => {
     if (promotedVideo) {
       promotedVideo.parentNode.removeChild(promotedVideo);
     }
+
+    if (config.speech) speech(mouseClick);
   } catch (e) {}
+};
+
+const speech = fakeClick => {
+  window.SpeechRecognition =
+    window.webkitSpeechRecognition || window.SpeechRecognition;
+
+  window.SpeechGrammarList =
+    window.webkitSpeechGrammarList || window.SpeechGrammarList;
+
+  let commands = ["pause", "stop", "play"];
+  let grammar =
+    "#JSGF V1.0; grammar commands; public <command> = " +
+    commands.join(" | ") +
+    " ;";
+  let recognition = new window.SpeechRecognition();
+  let speechRecognitionList = new window.SpeechGrammarList();
+
+  speechRecognitionList.addFromString(grammar, 1);
+
+  recognition.grammars = speechRecognitionList;
+  recognition.lang = "en-US";
+  recognition.interimResults = false;
+  recognition.maxAlternatives = 1;
+  recognition.continuous = true;
+
+  recognition.onresult = event => {
+    let last = event.results.length - 1;
+    let command = event.results[last][0].transcript;
+
+    command = command.trim().toLowerCase();
+
+    if (
+      command === "pause" ||
+      command === "stop" ||
+      command === "play" ||
+      command === "store" ||
+      command === "top"
+    ) {
+      let playButton = document.getElementsByClassName("ytp-play-button")[0];
+      playButton.dispatchEvent(fakeClick);
+    }
+
+    let indCommands = command.split();
+    if (indCommands.length > 1) {
+      if (
+        indCommands.indexOf("pause") > -1 ||
+        indCommands.indexOf("stop") > -1 ||
+        indCommands.indexOf("play") > -1 ||
+        indCommands.indexOf("store") > -1 ||
+        indCommands.top("store") > -1
+      ) {
+        let playButton = document.getElementsByClassName("ytp-play-button")[0];
+        playButton.dispatchEvent(fakeClick);
+      }
+    }
+  };
+  recognition.start();
 };
 
 document.addEventListener("DOMNodeInserted", handleAddNode);
